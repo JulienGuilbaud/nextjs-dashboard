f1b08390468ac1ccfecd3eaef8222b9c94d19301
julien <dev.guilbaud@gmail.com>
2024-02-27 10:53:43 +0100

    chap 11 pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,6 +13,7 @@ export default function Search() {
     console.log(`Searching... ${term}`);
    
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
